feec64665cead2b7be68717d93b6e4255e9ab35b
Alberto <xchao0213@gmail.com>
2021-01-13 16:00:30 +0800

    vconsole

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import App from './App.vue';
 import router from './router';
 import 'vant/lib/index.css';
 
-let vConsole = new VConsole();
-
 import wechat from './plugins/wechat';
 
 const app = createApp(App);
 app.config.globalProperties.$wechat = wechat;
+app.config.globalProperties.$console = new VConsole();
 
 app.use(Toast);
 app.use(router).mount('#app');
