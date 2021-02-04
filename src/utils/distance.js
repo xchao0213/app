5ee7457ff7ed5dd8d8c3f1d9b5dccf03e5ab36ea
Alberto <xchao0213@gmail.com>
2021-02-04 18:58:01 +0800

    n

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -35,7 +35,6 @@ export default function(fromLat, fromLon, toLat, toLon) {
   // } else {
   //   return measurePath(fromLat);
   // }
-  console.log('getDistance')
   const nfromLat = parseFloat(fromLat);
   const nfromLon = parseFloat(fromLon);
   const ntoLat = parseFloat(toLat);
